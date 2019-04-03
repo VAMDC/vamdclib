114d403784cba9b2f3a0f2a658473b488fb62aa2
Jacob Laas <jclaas@gmail.com>
2019-04-03 16:19:58 +0200

    updated the index  for the module contents

--- vamdclib/__init__.py
+++ vamdclib/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '0.1'
+__version__ = '0.3'
+__all__ = ["basemodel",
+           "database",
+           "functions",
+           "inchi",
+           "local_registry",
+           "nodes",
+           "query",
+           "registry",
+           "request",
+           "results",
+           "settings",
+           "specmodel"]
